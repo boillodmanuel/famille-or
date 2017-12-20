a887282ac55a112de3a0b6febca040b2363d0a41
Manuel Boillod <boillodmanuel@gmail.com>
2017-12-20 22:56:33 +0100

    add q

--- js/settings.js
+++ js/settings.js
@@ -1,56 +1,192 @@
 var questions = {
     1: {
-        question: 'Quelle est la couleur préférée des français ?',
+        question: 'Quelles sont nos sorties préférées ?',
         r1: {
-            text: 'bleu',
+            text: 'Le restaurant',
             score: 40
         },
         r2: {
-            text: 'vert',
+            text: 'Les bars',
             score: 20
         },
         r3: {
-            text: 'rouge',
+            text: 'Le théâtre',
             score: 10
         },
         r4: {
-            text: 'jaune',
+            text: 'Aller courir',
             score: 5
         },
         r5: {
-            text: 'blanc',
-            score: 1
+            text: 'Le cinéma',
+            score: 4
         },
         r6: {
-            text: 'violet',
+            text: 'Les poubelles',
             score: 1
         }
     },
     2: {
-        question: 'Quelles sont nos numéros préféres',
+        question: 'Quelles sont nos boissons préférées ?',
         r1: {
-            text: 'Le 1',
+            text: 'Bière',
             score: 35
         },
         r2: {
-            text: 'Le 2',
-            score: 25
+            text: 'Vin Rouge',
+            score: 33
         },
         r3: {
-            text: 'Le 3',
+            text: 'Rhum',
             score: 15
         },
         r4: {
-            text: 'Le 4',
+            text: 'L\'eau',
             score: 5
         },
         r5: {
-            text: 'Le 5',
+            text: 'Le coca zéro',
             score: 1
         },
         r6: {
-            text: 'Le 12',
+            text: 'Ricoré',
             score: 1
+        }
+    },
+    3: {
+        question: 'Quelle est la principale cause de dispute',
+        r1: {
+            text: 'La charge mentale',
+            score: 33
+        },
+        r2: {
+            text: 'Activités Élodie',
+            score: 20
+        },
+        r3: {
+            text: 'La belle-mère',
+            score: 11
+        },
+        r4: {
+            text: 'Les travaux',
+            score: 9
+        },
+        r5: {
+            text: 'Le ménage',
+            score: 8
+        },
+        r6: {
+            text: 'Les enfants',
+            score: 5
+        }
+    },
+    // 4: {
+    //     question: 'Quel sont nos applications mobiles préférées ?',
+    //     r1: {
+    //         text: 'La météo',
+    //         score: 21
+    //     },
+    //     r2: {
+    //         text: 'Spotify',
+    //         score: 17
+    //     },
+    //     r3: {
+    //         text: 'L\'agenda',
+    //         score: 14
+    //     },
+    //     r4: {
+    //         text: 'Les mails',
+    //         score: 12
+    //     },
+    //     r5: {
+    //         text: 'Les actualités',
+    //         score: 12
+    //     },
+    //     r6: {
+    //         text: 'Facebook',
+    //         score: 10
+    //     }
+    // },
+    4: {
+        question: 'Qu\'est ce qui à séduit Élodie au premier regard ?',
+        r1: {
+            text: 'Sa beauté',
+            score: 35
+        },
+        r2: {
+            text: 'Sa sympathie',
+            score: 23
+        },
+        r3: {
+            text: 'Sa sportivité',
+            score: 15
+        },
+        r4: {
+            text: 'Son humour(lourd)',
+            score: 7
+        },
+        r5: {
+            text: 'Sa festivité',
+            score: 5
+        },
+        r6: {
+            text: 'Sa Ford Escort',
+            score: 1
+        }
+    },
+    5: {
+        question: 'Nos séries TV préférées ?',
+        r1: {
+            text: 'How I met your mother',
+            score: 35
+        },
+        r2: {
+            text: 'Orange is a new black',
+            score: 21
+        },
+        r3: {
+            text: 'The Big Bang Theory',
+            score: 14
+        },
+        r4: {
+            text: 'Black Mirror',
+            score: 11
+        },
+        r5: {
+            text: 'Suits',
+            score: 9
+        },
+        r6: {
+            text: 'Desperate Housewife',
+            score: 5
+        }
+    },
+    6: {
+        question: 'Que font les Boillod plus vite que la moyenne ?',
+        coeff: 2,
+        r1: {
+            text: 'Le mariage',
+            score: 31
+        },
+        r2: {
+            text: 'Prépa marathon',
+            score: 20
+        },
+        r3: {
+            text: 'Résa vacances',
+            score: 16
+        },
+        r4: {
+            text: 'Acheter la maison',
+            score: 12
+        },
+        r5: {
+            text: 'L\'accouchement',
+            score: 5
+        },
+        r6: {
+            text: 'Le sexe',
+            score: 3
         }
     }
 };
